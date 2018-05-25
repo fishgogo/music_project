5000f10329c06180854f114533b62f2b2d3d379b
fishgogo <hugangmei@outlook.com>
2018-05-25 11:31:49 +0800

    music

---
+++ src/main.js
@@ -0,0 +1,35 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue';
+import App from './App';
+import router from './router';
+import $ from 'jquery';
+import axios from "axios";
+import VueAxios from "vue-axios";
+import Vuex from "vuex";
+
+Vue.config.productionTip = false
+
+Vue.use(VueAxios,axios);
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+	state:{
+		sound:[]
+	},
+	mutations:{
+		
+	},
+	actions:{
+
+	}
+})
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  store,
+  components: { App },
+  template: '<App/>'
+})
